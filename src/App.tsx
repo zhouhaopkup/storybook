4a847eac825453db4eaa4d56891cef826323124a
zhouhaopkup <18190816904@163.com>
2021-08-02 08:21:42 +0800

    feat: component Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import Button, { ButtonType, ButtonSize } from "./components/Button/Button";
 
 function App() {
   return (
     <>
-      <h1>Welcome</h1>
-      <div className="">Hello storybook!</div>
+      <Button className="custom">Hello</Button>
+      <Button autoFocus>Auto Focus</Button>
+      <Button disabled>Disabled</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        Large Primary
+      </Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+        Small Danger
+      </Button>
+      <Button
+        btnType={ButtonType.Link}
+        href="http://www.baidu.com"
+        target="_blank"
+      >
+        Link
+      </Button>
+      <Button disabled btnType={ButtonType.Link} href="http://www.baidu.com">
+        Link Disabled
+      </Button>
     </>
   );
 }
